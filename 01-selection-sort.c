a872090cce2363b6a8eb202dbf4605617b775ee0
saivishnu725 <saivishnu725@gmail.com>
2024-04-12 13:14:51 +0530

    Selection sort

---
+++ 01-selection-sort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void swap(int *a, int *b) {
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+}
+
+void display(int a[], int n) {
+		for(int i = 0; i < n; i++)
+				printf("%d ", a[i]);
+		printf("\n");
+}
+
+void sort(int a[], int n) {
+		int min;
+		for(int i = 0; i < n-1; i++) {
+				min = i;
+				for(int j = i+1; j < n; j++) {
+						if(a[j] < a[min])
+								min = j;
+				}
+				if(min != i) {
+						int temp = a[i];
+						a[i] = a[min];
+						a[min] = temp;
+				}
+						//swap(&a[min], &a[i]);
+		}
+}
+
+int main() {
+		int n;
+		printf("Enter the number of elements: ");
+		scanf("%d", &n);
+		int a[n];
+		printf("Enter %d elements: ", n);
+		for(int i = 0; i < n; i++)
+				scanf("%d", &a[i]);
+		sort(a, n);
+		printf("Sorted array: ");
+		display(a, n);
+		return 0;
+}
